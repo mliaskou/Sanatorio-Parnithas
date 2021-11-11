fe7b4116f970994a73f0d9a6b27a63412ea8d3a8
mliaskou <81467679+mliaskou@users.noreply.github.com>
2021-11-11 21:41:07 +0200

    Changes

--- Assets/Deer.cs
+++ Assets/Deer.cs
@@ -4,48 +4,25 @@ using UnityEngine;
 
 public class Deer : MonoBehaviour
 {
-    public float speed;
+    [RequireComponent(typeof(Animator))]
 
-    private Vector3 destination;
-    Animator anim;
-    public GameObject player;
-    
-    void Start()
+    public class RootMotionScript : MonoBehaviour
     {
-        // Set the destination to be the object's position so it will not start off moving
-        SetDestination(player.transform.position);
-        anim = GetComponent<Animator>();
-        
-    }
 
-    void Update()
-    {
-        // If the object is not at the target destination
-        if (destination != player.transform.position)
+        void OnAnimatorMove()
         {
-            // Move towards the destination each frame until the object reaches it
-            IncrementPosition();
+            Animator animator = GetComponent<Animator>();
+
+            if (animator)
+            {
+                Vector3 newPosition = transform.position;
+                newPosition.z += animator.GetFloat("Runspeed") * Time.deltaTime;
+                transform.position = newPosition;
+            }
         }
-    }
 
-    void IncrementPosition()
-    {
-        // Calculate the next position
-        float delta = speed * Time.deltaTime;
-        Vector3 currentPosition = gameObject.transform.position;
-        Vector3 nextPosition = Vector3.MoveTowards(currentPosition, destination, delta);
-
-        // Move the object to the next position
-        gameObject.transform.position = nextPosition;
-        
-    }
-
-    // Set the destination to cause the object to smoothly glide to the specified location
-    public void SetDestination(Vector3 value)
-    {
-        destination = value;
-        
     }
 }
-     
+
+
 
