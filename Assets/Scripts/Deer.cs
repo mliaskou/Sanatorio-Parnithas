67b9a55b5185eabf3af738eb21b114270b499c68
mliaskou <81467679+mliaskou@users.noreply.github.com>
2022-06-09 15:19:31 +0300

    Changes2

--- Assets/Scripts/Deer.cs
+++ Assets/Scripts/Deer.cs
@@ -1,10 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Deer : MonoBehaviour
 {
-    [RequireComponent(typeof(Animator))]
+
+    NavMeshAgent nma;
+    public GameObject player;
+    Animator anim;
+
+
+    private void Start()
+    {
+        nma = this.GetComponent<NavMeshAgent>();
+        anim = this.GetComponent<Animator>();
+        nma.updatePosition = false;
+        nma.updateRotation = true;
+    }
+
+
+    private void Update()
+    {
+        nma.SetDestination(player.transform.position);
+        anim.SetFloat("Speed", 1f);
+
+
+
+
+             float distance = Vector3.Distance(nma.destination, player.transform.position);
+
+        if (distance > 1f)
+        {
+            // Play walk anim
+            anim.SetFloat("SpeedY", 1);
+        }
+        else
+        {
+            // Play idle anim
+            anim.SetFloat("SpeedY", 0);
+        }
+
+        nma.nextPosition = anim.rootPosition;
+    }
+
+    /* [RequireComponent(typeof(Animator))]
 
     public class RootMotionScript : MonoBehaviour
     {
@@ -21,7 +61,7 @@ public class Deer : MonoBehaviour
             }
         }
 
-    }
+    }*/
 }
 
 
